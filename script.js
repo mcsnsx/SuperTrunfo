828d8d9b795c46bb7e4ad33a976dee48d9dec904
Maria Carolina Xavier <mcs_nasc@hotmail.com>
2022-06-21 21:07:33 -0300

    Cartas

--- script.js
+++ script.js
@@ -1,43 +1,220 @@
 var cartaPikachu = {
     nome: "Pikachu",
-    imagem: "https://s.aficionados.com.br/imagens/shiryu.jpg",
+    imagem: "https://www.purarteadesivos.com.br/wp-content/uploads/2016/11/adesivo-personalizado-pikachu-de-paredes-home-office-carro-sticker-pokemon-geek-nerd-pura-arte-adesivos.png",
     atributos: {
-      Ataque: 80,
+      Ataque: 99,
       Defesa: 40,
-      Magia: 60
+      Magia: 70
     }
   };
   var cartaBulbassauro = {
     nome: "Bulbassauro",
-    imagem: "https://www.creativeuncut.com/gallery-09/art/pmdbrt-bulbasaur.jpg",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQaSPqeh8s2aVaMI4Tow_fc_l9LJ0SlAfYDHrjzRZCe5RMQC5YqbeIcS7Rq-mWsXIl-aw&usqp=CAU",
     atributos: {
-      Ataque: 7,
-      Defesa: 8,
-      Magia: 6
+      Ataque: 70,
+      Defesa: 80,
+      Magia: 40
     }
   };
   
   var cartaCharmander = {
     nome: "Charmander",
     imagem:
-      "https://disneyplusbrasil.com.br/wp-content/uploads/2021/06/Darth-Vader-serie-Disney-Plus-1024x576.jpg",
+      "https://i.pinimg.com/originals/dc/75/b9/dc75b96b4141c0a0f5d2658b084e170b.png",
     atributos: {
-      Ataque: 9,
-      Defesa: 8,
-      Magia: 2
+      Ataque: 10,
+      Defesa: 40,
+      Magia: 10
     }
   };
 
   var cartaSquirtle = {
     nome: "Squirtle",
     imagem:
-      "https://disneyplusbrasil.com.br/wp-content/uploads/2021/06/Darth-Vader-serie-Disney-Plus-1024x576.jpg",
+      "https://mestrepokemon.com/wp-content/uploads/2019/11/Squirtle-Pok%C3%A9dex.jpg",
+    atributos: {
+      Ataque: 70,
+      Defesa: 80,
+      Magia: 30
+    }
+  };
+
+  var cartaCaterpie = {
+    nome: "Carterpie",
+    imagem:
+      "https://shop7.webmodule.prestashop.net/pokedoge/11297-large_default/caterpie.jpg",
+    atributos: {
+      Ataque: 30,
+      Defesa: 80,
+      Magia: 40
+    }
+  };
+
+  var cartaWeedle = {
+    nome: "Weedle",
+    imagem:
+      "https://www.pngitem.com/pimgs/m/222-2222659_pokemon-weedle-transparent-png-pokemon-weedle-png-download.png",
+    atributos: {
+      Ataque: 50,
+      Defesa: 70,
+      Magia: 40
+    }
+  };
+
+  var cartaPidgey = {
+    nome: "Pidgey",
+    imagem:
+      "https://mestrepokemon.com/wp-content/uploads/2020/03/Pok%C3%A9mon-Pidgey-Pok%C3%A9dex.png",
     atributos: {
       Ataque: 90,
+      Defesa: 60,
+      Magia: 40
+    }
+  };
+
+  var cartaRattata = {
+    nome: "Rattata",
+    imagem:
+      "https://pokemonov.net/assets/images/pokemony/19-rattata.png",
+    atributos: {
+      Ataque: 80,
+      Defesa: 50,
+      Magia: 30
+    }
+  };
+
+  var cartaSpearow = {
+    nome: "Spearow",
+    imagem:
+      "http://pm1.narvii.com/6371/ccd3243fb85d035abb0c175c7a75b293113e6309_00.jpg",
+    atributos: {
+      Ataque: 90,
+      Defesa: 30,
+      Magia: 30
+    }
+  };
+
+  var cartaEkans = {
+    nome: "Ekas",
+    imagem:
+      "https://i.pinimg.com/originals/22/cc/02/22cc02ceb7b3d083116f8f60be5f204f.jpg",
+    atributos: {
+      Ataque: 80,
+      Defesa: 50,
+      Magia: 70
+    }
+  };
+
+  var cartaNidoran = {
+    nome: "Nidoran",
+    imagem:
+      "https://shop7.webmodule.prestashop.net/pokedoge/11385-large_default/nidoran-m.jpg",
+    atributos: {
+      Ataque: 50,
       Defesa: 80,
       Magia: 20
     }
   };
+
+  var cartaClefairy = {
+    nome: "Clefairy",
+    imagem:
+      "https://shop7.webmodule.prestashop.net/pokedoge/11399-home_default/clefairy.jpg",
+    atributos: {
+      Ataque: 10,
+      Defesa: 70,
+      Magia: 90
+    }
+  };
+
+  var cartaJigglypuff = {
+    nome: "Jigglypuff",
+    imagem:
+      "https://i.pinimg.com/564x/dc/e7/66/dce766b16be44891e3e773a70f140c4d--red-flare-game-night.jpg",
+    atributos: {
+      Ataque: 30,
+      Defesa: 50,
+      Magia: 90
+    }
+  };
+
+  var cartaZubat = {
+    nome: "Zubat",
+    imagem:
+      "https://i.pinimg.com/564x/04/ba/d3/04bad30757cc64a63f352077c1c38e98--zubat-pokemon--pokemon.jpg",
+    atributos: {
+      Ataque: 60,
+      Defesa: 50,
+      Magia: 70
+    }
+  };
+
+  var cartaOddish = {
+    nome: "Oddish",
+    imagem:
+      "https://i.pinimg.com/474x/b5/28/8d/b5288d19573509b1338d09aa71e0a608.jpg",
+    atributos: {
+      Ataque: 60,
+      Defesa: 80,
+      Magia: 20
+    }
+  };
+
+  var cartaMeowth = {
+    nome: "Meowth",
+    imagem:
+      "https://i.pinimg.com/originals/98/2a/b6/982ab66f09a7cc30332ebd73cb4ab4fc.png",
+    atributos: {
+      Ataque: 80,
+      Defesa: 80,
+      Magia: 30
+    }
+  };
+
+  var cartaPoliwag = {
+    nome: "Poliwag",
+    imagem:
+      "https://pokemon-revenge-world.weebly.com/uploads/1/6/2/0/16202468/717227.jpg",
+    atributos: {
+      Ataque: 50,
+      Defesa: 80,
+      Magia: 60
+    }
+  };
+
+  var cartaGrowlithe = {
+    nome: "Growlithe",
+    imagem:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSi9yXVANvhE7OJBszRL4a8OC2iKFS1ecBLeq-ZCB4kHfb9jPQLHqjmCVHa67qPh6t_qXQ&usqp=CAU",
+    atributos: {
+      Ataque: 90,
+      Defesa: 70,
+      Magia: 30
+    }
+  };
+
+  var cartaOnix = {
+    nome: "Onix",
+    imagem:
+      "https://tekgenius.pt/wp-content/uploads/2017/02/Onix.png",
+    atributos: {
+      Ataque: 80,
+      Defesa: 90,
+      Magia: 10
+    }
+  };
+
+  var cartaEevee = {
+    nome: "Eevee",
+    imagem:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-iChigIgDmJfyUjB2PgPm4a07J3UaC7l0m3-x30PmvFsXg5lda3Tmz6qGaStoeNAJ0rg&usqp=CAU",
+    atributos: {
+      Ataque: 60,
+      Defesa: 80,
+      Magia: 50
+    }
+  };
+
   
   var cartaMaquina;
   var cartaJogador;
@@ -45,12 +222,12 @@ var cartaPikachu = {
     cartaPikachu, 
     cartaBulbassauro, 
     cartaCharmander,
-    cartaSquirtle/*,
+    cartaSquirtle,
     cartaCaterpie,
     cartaWeedle,
     cartaPidgey,
     cartaRattata,
-    cartaFearow,
+    cartaSpearow,
     cartaEkans,
     cartaNidoran,
     cartaClefairy,
@@ -60,7 +237,8 @@ var cartaPikachu = {
     cartaMeowth,
     cartaPoliwag,
     cartaGrowlithe,
-    cartaOnix,*/
+    cartaOnix,
+    cartaEevee
   ];
   
   function sortearCarta() {
